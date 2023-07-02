1fd4b68fa70a3bcf6be984365a2d7f4177cbfa83
KienMing <tankienming0916@e.newera.edu.my>
2023-07-02 20:06:21 +0800

    add brand logo

--- project/menu.php
+++ project/menu.php
@@ -1,19 +1,25 @@
-<nav class="nav navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand ps-5" href="#">KM</a>
-            <div class="collapse navbar-collapse justify-content-end pe-3">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="product_create.php">Create Product</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="customer_create.php">Create Customer</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contactus.php">Contact Us</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="d-flex align-items-center ms-1">
+        <img src="img/factorylogo.png" alt="factorylogo" width="50" height="40" class="d-inline-block align-text-top ms-3">
+        <a class="navbar-brand ms-2" href="#">KM</a>
+    </div>
+    <button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end pe-3" id="navbarNav">
+        <ul class="navbar-nav ps-3">
+            <li class="nav-item p-1">
+                <a class="nav-link active" href="index.php">Home</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link" href="product_create.php">Create Product</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link" href="customer_create.php">Create Customer</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link" href="contactus.php">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
