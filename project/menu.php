d587108717a2323450d009cc31e06bb615e3f4fe
KienMing <tankienming0916@e.newera.edu.my>
2023-07-14 21:41:30 +0800

    create dropdown menu for product and customer

--- project/menu.php
+++ project/menu.php
@@ -1,3 +1,11 @@
+<?php
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="d-flex align-items-center ms-1">
         <img src="img/factorylogo.png" alt="factorylogo" width="50" height="40" class="d-inline-block align-text-top ms-3">
@@ -11,21 +19,27 @@
             <li class="nav-item p-1">
                 <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == '/webapplicationdevelopment/project/index.php') echo 'active'; ?>" href="index.php">Home</a>
             </li>
-            <li class="nav-item p-1">
-                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == '/webapplicationdevelopment/project/product_create.php') echo 'active'; ?>" href="product_create.php">Create Product</a>
+            <li class="nav-item p-1 dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="productDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Product</a>
+                <ul class="dropdown-menu" aria-labelledby="productDropdown">
+                    <li><a class="dropdown-item" href="product_create.php">Create Product</a></li>
+                    <li><a class="dropdown-item" href="product_read.php">Read Product</a></li>
+                </ul>
             </li>
-            <li class="nav-item p-1">
-                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == '/webapplicationdevelopment/project/product_read.php') echo 'active'; ?>" href="product_read.php">Read Product</a>
-            </li>
-            <li class="nav-item p-1">
-                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == '/webapplicationdevelopment/project/customer_create.php') echo 'active'; ?>" href="customer_create.php">Create Customer</a>
-            </li>
-            <li class="nav-item p-1">
-                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == '/webapplicationdevelopment/project/customer_read.php') echo 'active'; ?>" href="customer_read.php">Read Customer</a>
+            <li class="nav-item p-1 dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="customerDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Customer</a>
+                <ul class="dropdown-menu" aria-labelledby="customerDropdown">
+                    <li><a class="dropdown-item" href="customer_create.php">Create Customer</a></li>
+                    <li><a class="dropdown-item" href="customer_read.php">Read Customer</a></li>
+                </ul>
             </li>
             <li class="nav-item p-1">
                 <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == '/webapplicationdevelopment/project/contactus.php') echo 'active'; ?>" href="contactus.php">Contact Us</a>
             </li>
+            <li class="nav-item p-1">
+                <a class="nav-link" href="?logout=true">Logout</a>
+            </li>
         </ul>
     </div>
 </nav>
+
