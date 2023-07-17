27d52f03b44f56d2a427a6b98e84cc8232f07636
KienMing <tankienming0916@e.newera.edu.my>
2023-07-17 16:43:52 +0800

    create file

---
+++ project/category_read.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: login.php");
+  exit();
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>List of Categories</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container p-0 bg-light">
+        <?php
+            include 'menu/menu.php';
+        ?>
+
+        <div class="page-header p-3 pb-1">
+            <h1>Read Categories</h1>
+        </div>
+     
+        <?php
+        include 'config/database.php';
+
+        $searchKeyword = isset($_GET['search']) ? $_GET['search'] : '';
+        $query = "SELECT Category_ID, category_name, description FROM categories";
+        if (!empty($searchKeyword)) {
+            $query .= " WHERE category_name LIKE :keyword";
+            $searchKeyword = "%{$searchKeyword}%";
+        }
+        $query .= " ORDER BY Category_ID ASC";
+        $stmt = $con->prepare($query);
+        if (!empty($searchKeyword)) {
+            $stmt->bindParam(':keyword', $searchKeyword);
+        }
+
+        $stmt->execute();
+        $num = $stmt->rowCount();
+
+        echo '<div class="p-3 pt-2">
+            <a href="category_create.php" class="btn btn-primary m-b-1em">Create New Category</a>
+        </div>';
+
+        echo '<div class="p-3">
+            <form method="GET" action="">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="search" placeholder="Search product..." value="' . str_replace('%', '', $searchKeyword) . '">
+                    <button class="btn btn-primary" type="submit">Search</button>
+                </div>
+            </form>
+        </div>';
+        
+        if($num > 0){
+            echo "<div class='p-3'>";
+                echo "<table class='table table-hover table-responsive table-bordered'>";//start table
+                echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>Category Name</th>";
+                    echo "<th>Description</th>";
+                    echo "<th>Action</th>";
+                echo "</tr>";
+
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    extract($row);
+                    echo "<tr>";
+                        echo "<td>{$Category_ID}</td>";
+                        echo "<td class='col-2'>{$category_name}</td>";
+                        echo "<td>{$description}</td>";
+
+                        echo "<td class='col-3'>";
+                            //tempo
+                            echo "<a href='category_read_one.php?id={$Category_ID}' class='btn btn-info m-r-1em text-white mx-2'>Read</a>";
+                            echo "<a href='product_update.php?id={$Category_ID}' class='btn btn-primary m-r-1em mx-2'>Edit</a>";
+                            echo "<a href='#' onclick='delete_product({$Category_ID});'  class='btn btn-danger mx-2'>Delete</a>";
+                        echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            echo "</div>";    
+        }else{
+            echo '<div class="p-3">
+                <div class="alert alert-danger">No records found.</div>
+            </div>';
+        }
+        ?>   
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ project/category_read_one.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: login.php");
+  exit();
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Read Category</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container p-0 bg-light">
+        <?php
+            include 'menu/menu.php';
+        ?>
+
+        <div class="page-header p-3 pb-1">
+            <h1>Read Category</h1>
+        </div>
+         
+        <?php
+        $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+        include 'config/database.php';
+        try {
+            // prepare select query
+            $query = "SELECT Category_ID, category_name, description FROM categories WHERE Category_ID = :id ";
+            $stmt = $con->prepare($query);
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+            $name = $row['category_name'];
+            $description = $row['description'];
+        }
+        
+        catch(PDOException $exception){
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+        
+        <div class="p-3">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Category Name</td>
+                    <td><?php echo htmlspecialchars($name, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <a href='category_read.php' class='btn btn-danger'>Back to category list</a>
+                    </td>
+                </tr>
+            </table>
+        </div>   
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
