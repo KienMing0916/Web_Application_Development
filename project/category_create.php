cf5be8b07e95f8dd01870b44e9a17f6d2c995994
KienMing <tankienming0916@e.newera.edu.my>
2023-07-17 16:43:35 +0800

    create file

---
+++ project/category_create.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: login.php");
+  exit();
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Create Category</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>  
+    <div class="container p-0 bg-light">
+        <?php
+            include 'menu/menu.php';
+        ?>
+
+        <div class="page-header p-3 pb-1">
+            <h1>Create Category</h1>
+        </div>
+      
+        <?php
+        if($_POST){
+            include 'config/database.php';
+            try{
+                // insert query
+                $query = "INSERT INTO categories SET category_name=:name, description=:description";
+                // prepare query for execution
+                $stmt = $con->prepare($query);
+                $name = $_POST['name'];
+                $description = $_POST['description'];
+                $errorMessage = array();
+
+                if(empty($name)) {
+                    $errorMessage[] = "Name field is empty.";
+                }
+                if(empty($description)) {
+                    $errorMessage[] = "Description field is empty.";
+                }
+
+
+                if(!empty($errorMessage)) {
+                    echo "<div class='alert alert-danger m-3'>";
+                        foreach ($errorMessage as $displayErrorMessage) {
+                            echo $displayErrorMessage . "<br>";
+                        }
+                    echo "</div>";
+                }else {
+                    // Bind the parameters
+                    $stmt->bindParam(':name', $name);
+                    $stmt->bindParam(':description', $description);
+                    
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success m-3'>Record was saved.</div>";
+                        $_POST = array();
+                    } else {
+                        echo "<div class='alert alert-danger m-3'>Unable to save the record.</div>";
+                    }
+                }
+            }
+            catch(PDOException $exception){
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+        <div class="p-3">
+            <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST">
+                <table class='table table-hover table-responsive table-bordered'>
+                    <tr>
+                        <td class="col-4">Category Name</td>
+                        <td class="col-8"><input type='text' name='name' id='name' class='form-control' value="<?php echo isset($_POST['name']) ? $_POST['name'] : ''; ?>" /></td>
+                    </tr>
+                    <tr>
+                        <td>Description</td>
+                        <td><textarea name='description' id='description' class='form-control'><?php echo isset($_POST['description']) ? $_POST['description'] : ''; ?></textarea></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type='submit' value='Save' class='btn btn-primary' />
+                            <a href='category_read.php' class='btn btn-danger'>Back to read categories</a>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
