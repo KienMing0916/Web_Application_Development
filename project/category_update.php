43402cc76e1fe188b39d17c30e7546955026541b
KienMing <tankienming0916@e.newera.edu.my>
2023-07-24 21:53:04 +0800

    wrote a function to handle error message

---
+++ project/category_update.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: login.php");
+  exit();
+}
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>Update Category Details</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container bg-light">
+        <?php
+            include 'menu/nav.php';
+        ?>
+
+        <div class="page-header p-3 pb-1">
+            <h1>Update Category</h1>
+        </div>
+
+        <?php
+            $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+            include 'config/database.php';
+        try {
+            $query = "SELECT Category_ID, category_name, description FROM categories WHERE Category_ID = ? LIMIT 0,1";
+            $stmt = $con->prepare($query);
+            $stmt->bindParam(1, $id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $category_name = $row['category_name'];
+            $description = $row['description'];
+        }
+        catch(PDOException $exception){
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+        <?php
+        if($_POST){
+            try{
+                $query = "UPDATE categories SET category_name=:category_name, description=:description WHERE Category_ID = :id";
+                $stmt = $con->prepare($query);
+
+                $category_name = htmlspecialchars(strip_tags($_POST['category_name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+
+                include 'menu/common_use_function.php';
+                $errorMessage = validateCategoryForm($category_name, $description);
+
+
+                if(!empty($errorMessage)) {
+                    echo "<div class='alert alert-danger m-3'>";
+                        foreach ($errorMessage as $displayErrorMessage) {
+                            echo $displayErrorMessage . "<br>";
+                        }
+                    echo "</div>";
+                }else {
+                    $stmt->bindParam(':category_name', $category_name);
+                    $stmt->bindParam(':description', $description);
+                    $stmt->bindParam(':id', $id);
+                    // Execute the query
+                    if($stmt->execute()){
+                        echo "<div class='alert alert-success m-3'>Record was updated.</div>";
+
+                    }else{
+                        echo "<div class='alert alert-danger m-3'>Unable to update record. Please try again.</div>";
+                    }  
+                }       
+            }
+            catch(PDOException $exception){
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+        <form class="p-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}");?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td class="col-4">Category Name</td>
+                    <td><input type='text' name='category_name' value="<?php echo htmlspecialchars($category_name, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save Changes' class='btn btn-primary' />
+                        <a href='category_read.php' class='btn btn-danger'>Back to category list</a>
+                    </td>
+                </tr>
+            </table>
+        </form>     
+   </div> 
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
---
+++ project/product_update.php
@@ -0,0 +1,161 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: login.php");
+  exit();
+}
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>Update Product Details</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container bg-light">
+        <?php
+            include 'menu/nav.php';
+        ?>
+
+        <div class="page-header p-3 pb-1">
+            <h1>Update Product</h1>
+        </div>
+
+        <?php
+            $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+            include 'config/database.php';
+        try {
+            // prepare select query
+            $query = "SELECT Product_ID, name, description, price, promotion_price, manufacture_date, expired_date, Category_ID FROM products WHERE Product_ID = ? LIMIT 0,1";
+            $stmt = $con->prepare($query);
+            // this is the first question mark
+            $stmt->bindParam(1, $id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            // values to fill up our form
+            $name = $row['name'];
+            $description = $row['description'];
+            $price = $row['price'];
+            $promotion_price = $row['promotion_price'];
+            $manufacture_date = $row['manufacture_date'];
+            $expired_date = $row['expired_date'];
+            $category_id = $row['Category_ID'];
+        }
+        catch(PDOException $exception){
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+        <?php
+        if($_POST){
+            try{
+                $query = "UPDATE products SET name=:name, description=:description, price=:price, promotion_price=:promotion_price, manufacture_date=:manufacture_date, expired_date=:expired_date, Category_ID=:category_id WHERE Product_ID = :id";
+                $stmt = $con->prepare($query);
+                // posted values
+                $name = htmlspecialchars(strip_tags($_POST['name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+                $price = htmlspecialchars(strip_tags($_POST['price'])); 
+                $promotion_price = htmlspecialchars(strip_tags($_POST['promotion_price']));
+                $manufacture_date = htmlspecialchars(strip_tags($_POST['manufacture_date']));
+                $expired_date = htmlspecialchars(strip_tags($_POST['expired_date'])); 
+                $category_id = htmlspecialchars(strip_tags($_POST['category_id'])); 
+
+                include 'menu/common_use_function.php';
+                $errorMessage = validateProductForm($name, $description, $price, $promotion_price, $manufacture_date, $expired_date, $category_id);
+
+
+                if(!empty($errorMessage)) {
+                    echo "<div class='alert alert-danger m-3'>";
+                        foreach ($errorMessage as $displayErrorMessage) {
+                            echo $displayErrorMessage . "<br>";
+                        }
+                    echo "</div>";
+                }else {
+                    $stmt->bindParam(':name', $name);
+                    $stmt->bindParam(':description', $description);
+                    $stmt->bindParam(':price', $price);
+                    $stmt->bindParam(':id', $id);
+                    $stmt->bindParam(':promotion_price', $promotion_price);
+                    $stmt->bindParam(':manufacture_date', $manufacture_date);
+                    $stmt->bindParam(':expired_date', $expired_date);
+                    $stmt->bindParam(':category_id', $category_id);
+                    // Execute the query
+                    if($stmt->execute()){
+                        echo "<div class='alert alert-success m-3'>Record was updated.</div>";
+
+                    }else{
+                        echo "<div class='alert alert-danger m-3'>Unable to update record. Please try again.</div>";
+                    }  
+                }       
+            }
+            catch(PDOException $exception){
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+        <form class="p-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}");?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td class="col-4">Name</td>
+                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td><input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+
+                <tr>
+                    <td>Promotion Price</td>
+                    <td><input type='text' name='promotion_price' value="<?php echo htmlspecialchars($promotion_price, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Manufacture Date</td>
+                    <td><input type='date' name='manufacture_date' value="<?php echo htmlspecialchars($manufacture_date, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Expired Date</td>
+                    <td><input type='date' name='expired_date' value="<?php echo htmlspecialchars($expired_date, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Category</td>
+                    <td>
+                        <select name="category_id" class="form-select">
+                            <?php
+                            include 'config/database.php';
+                            // Fetch categories from the database
+                            $categoryQuery = "SELECT category_id, category_name FROM categories";
+                            $categoryStmt = $con->prepare($categoryQuery);
+                            $categoryStmt->execute();
+
+                            while ($row = $categoryStmt->fetch(PDO::FETCH_ASSOC)) {
+                                $categoryIDFromCategoriesTable = $row['category_id'];
+                                $categoryName = $row['category_name'];
+
+                                $selected = ($categoryIDFromCategoriesTable == $category_id) ? 'selected' : '';
+                                echo "<option value='" . htmlspecialchars($categoryIDFromCategoriesTable, ENT_QUOTES) . "' $selected>" . htmlspecialchars($categoryName, ENT_QUOTES) . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save Changes' class='btn btn-primary' />
+                        <a href='product_read.php' class='btn btn-danger'>Back to product list</a>
+                    </td>
+                </tr>
+            </table>
+        </form>     
+   </div> 
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
