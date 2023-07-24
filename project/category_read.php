c4007807f328ee93f90be1f785d5948deba6ee92
KienMing <tankienming0916@e.newera.edu.my>
2023-07-24 21:51:01 +0800

    change placeholder text

--- project/category_read.php
+++ project/category_read.php
@@ -47,7 +47,7 @@ if (!isset($_SESSION['user_id'])) {
         echo '<div class="p-3">
             <form method="GET" action="">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" name="search" placeholder="Search product..." value="' . str_replace('%', '', $searchKeyword) . '">
+                    <input type="text" class="form-control" name="search" placeholder="Search category..." value="' . str_replace('%', '', $searchKeyword) . '">
                     <button class="btn btn-primary" type="submit">Search</button>
                 </div>
             </form>
