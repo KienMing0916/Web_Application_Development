4acfe5b2dc3d5eaf83b927ecc0bae845cc9c85a8
KienMing <tankienming0916@e.newera.edu.my>
2023-07-25 23:01:38 +0800

    write a function for validate update customer

--- project/menu/common_use_function.php
+++ project/menu/common_use_function.php
@@ -58,11 +58,27 @@ function validateCategoryForm($category_name, $description) {
     return $errorMessage;
 }
 
-function validateCustomerForm($username, $firstname, $lastname, $gender, $birthdate, $email, $status) {
+function validateCreateCustomerForm($username, $password, $confirmpassword, $firstname, $lastname, $gender, $birthdate, $email, $status) {
     $errorMessage = array();
+    $passwordPattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/";
 
     if(empty($username)) {
         $errorMessage[] = "Username field is empty.";
+    }
+    if(empty($password)) {
+        $errorMessage[] = "Password field is empty.";
+    }else {
+        if (!preg_match($passwordPattern, $password)) {
+            $errorMessage[] = "Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number. No special symbols allowed.";
+        }
+    }
+    if (empty($confirmpassword)) {
+        $errorMessage[] = "Confirm password field is empty.";
+    }
+    if (!empty($password) && !empty($confirmpassword)) {
+        if ($password !== $confirmpassword) {
+            $errorMessage[] = "Password and confirm password do not match.";
+        }
     }
     if(empty($firstname)) {
         $errorMessage[] = "First name field is empty.";
@@ -95,27 +111,57 @@ function validateCustomerForm($username, $firstname, $lastname, $gender, $birthd
     return $errorMessage;
 }
 
-function validateCustomerFormPassword($password, $confirmpassword) {
+function validateUpdateCustomerForm($username, $firstname, $lastname, $gender, $birthdate, $email, $status, $db_password, $current_password, $new_password, $confirm_new_password) {
     $errorMessage = array();
+    $passwordPattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/";
 
-    if(empty($password)) {
-        $errorMessage[] = "Password field is empty.";
+    if(empty($username)) {
+        $errorMessage[] = "Username field is empty.";
+    }
+    if(empty($firstname)) {
+        $errorMessage[] = "First name field is empty.";
+    }
+    if(empty($lastname)) {
+        $errorMessage[] = "Last name field is empty.";
+    }
+    if(empty($gender)) {
+        $errorMessage[] = "Gender field is empty.";
+    }
+    if(empty($birthdate)) {
+        $errorMessage[] = "Date of birth field is empty.";
     }else {
-        $passwordPattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/";
-        if (!preg_match($passwordPattern, $password)) {
+        $currentDate = date('Y-m-d');
+        if($birthdate > $currentDate) {
+            $errorMessage[] = "Date of birth cannot be in the future.";
+        }
+    }
+    if(empty($email)) {
+        $errorMessage[] = "Email field is empty.";
+    }else{
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errorMessage[] = "Invalid email format.";
+        }
+    }
+    if(empty($status)) {
+        $errorMessage[] = "Account status field is empty.";
+    }
+    if (empty($current_password) && empty($new_password) && empty($confirm_new_password)) {
+        return $errorMessage;
+    }
+    if (empty($current_password) || empty($new_password) || empty($confirm_new_password)) {
+        $errorMessage[] = "If you wish to change your password, please fill in all three password fields.";
+        return $errorMessage;
+    } else {
+        if (!password_verify($current_password, $db_password)) {
+            $errorMessage[] = "Incorrect current password.";
+        }
+        if (!preg_match($passwordPattern, $new_password)) {
             $errorMessage[] = "Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number. No special symbols allowed.";
         }
-    }
-    if (empty($confirmpassword)) {
-        $errorMessage[] = "Confirm password field is empty.";
-    }
-    if (!empty($password) && !empty($confirmpassword)) {
-        if ($password !== $confirmpassword) {
-            $errorMessage[] = "Password and confirm password do not match.";
+        if ($new_password !== $confirm_new_password) {
+            $errorMessage[] = "New password and confirm password do not match.";
         }
+        return $errorMessage;
     }
-
-    return $errorMessage;
 }
-
 ?>
