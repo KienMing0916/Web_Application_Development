fb4e02f33bbd5af0c7a73531bf960ca5ab0263e8
KienMing <tankienming0916@e.newera.edu.my>
2023-07-30 15:21:02 +0800

    add link for button

--- project/index.php
+++ project/index.php
@@ -39,7 +39,7 @@ include 'menu/validate_login.php';
         <div class="parttwo bg-light text-center pt-4 pb-4">
             <h1>Welcome to XXX Supermarket</h1>
             <p class="lead">Welcome to our dynamic and innovative supermarket! We are dedicated to providing a seamless shopping experience and connecting customers with a wide range of quality products from around the world. Our goal is to make your grocery shopping hassle-free and enjoyable.</p>
-            <a href="create_order.php" class="btn btn-primary btn-lg">Make an order now</a>
+            <a href="order_create.php" class="btn btn-primary btn-lg">Make an order now</a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
