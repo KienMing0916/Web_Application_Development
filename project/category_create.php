b97f6b6194cd425c9a07c52bd56ecfaad0adfef7
KienMing <tankienming0916@e.newera.edu.my>
2023-08-27 14:33:26 +0800

    rename head title

--- project/category_create.php
+++ project/category_create.php
@@ -5,7 +5,7 @@ include 'menu/validate_login.php';
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Create Category</title>
+    <title>Add New Category</title>
     <link rel="icon" type="image/x-icon" href="img/factorylogo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
