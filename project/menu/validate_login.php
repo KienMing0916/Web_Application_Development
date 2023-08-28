51817a1157d07f4e91c936f9a0a7cfce7e1058f5
KienMing <tankienming0916@e.newera.edu.my>
2023-08-28 14:46:11 +0800

    rename file

--- project/menu/validate_login.php
+++ project/menu/validate_login.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 if (!isset($_SESSION['Customer_ID'])) {
-    header("Location: login.php?action=warning");
+    header("Location: index.php?action=warning");
     exit();
 }
 
 if (isset($_GET['logout'])) {
     session_destroy();
-    header("Location: login.php");
+    header("Location: index.php");
     exit();
 }
 ?>
